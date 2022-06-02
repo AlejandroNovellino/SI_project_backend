43218337019e8ff193d3baeec4878a187e6245e4
Alejandro Novellino <alnovellino07@gmail.com>
2022-06-01 21:56:52 -0400

    get and post created for artist, project and version

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Artist, Project, ProjectVersion, FileID, Poll
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +30,93 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/artist', methods=['GET', 'POST'])
+def artist():
+    '''
+        POST: create the artist based on the info give it
+        GET: get the artist based on the username and password combination
+    '''
+    # request of type POST
+    if request.method == 'POST':
+        try:
+            data = request.json
+            # create the user
+            artist = Artist.create(**data)
+            if not isinstance(artist, Artist):
+                return jsonify({"msg": "Artist could not be created"}), 500
+            return jsonify({"msg": "Artist created successfully"}), 201
+        except:
+            return jsonify({"msg": "Error with the request data"}), 400
+    # request of type GET
+    elif request.method == 'GET':
+        try:
+            data = request.json
+            # get the artist by username and password
+            artist = Artist.query.filter_by(username=data["username"], password=data["password"]).first()
+            if not artist:
+                return jsonify({"msg": "Combination is not valid"}), 400
+            return jsonify({"artist": artist.serialize()}), 200
+        except:
+            return jsonify({"msg": "Error with the request data"}), 400
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/project', methods=['GET', 'POST'])
+def project():
+    '''
+        POST: create the project based on the artist id and project info
+        GET: get the project based on the id
+    '''
+    # request of type POST
+    if request.method == 'POST':
+        try:
+            data = request.json
+            # create the user
+            project = Project.create(**data)
+            if not isinstance(project, Project):
+                return jsonify({"msg": "Project could not be created"}), 500
+            return jsonify({"msg": "Project created successfully"}), 201
+        except:
+            return jsonify({"msg": "Error with the request data"}), 400
+    # request of type GET
+    elif request.method == 'GET':
+        try:
+            data = request.json
+            # get the artist by username and password
+            project = Project.query.filter_by(id=data["id"]).first()
+            print(project)
+            if not project:
+                return jsonify({"msg": "Project with the id established does not exist"}), 400
+            return jsonify({"project": project.serialize()}), 200
+        except:
+            return jsonify({"msg": "Error with the request data"}), 400
 
-    return jsonify(response_body), 200
+@app.route('/project-version', methods=['GET', 'POST'])
+def project_version():
+    '''
+        POST: create the project version based on the project id and project version info
+        GET: get the project version based on the id
+    '''
+    # request of type POST
+    if request.method == 'POST':
+        try:
+            data = request.json
+            # create the user
+            project_version = ProjectVersion.create(**data)
+            if not isinstance(project_version, ProjectVersion):
+                return jsonify({"msg": "Project version could not be created"}), 500
+            return jsonify({"msg": "Project version created successfully"}), 201
+        except:
+            return jsonify({"msg": "Error with the request data"}), 400
+    # request of type GET
+    elif request.method == 'GET':
+        try:
+            data = request.json
+            # get the artist by username and password
+            project_version = ProjectVersion.query.filter_by(id=data["id"]).first()
+            if not project_version:
+                return jsonify({"msg": "Project version with the id established does not exist"}), 400
+            return jsonify({"project_version": project_version.serialize()}), 200
+        except:
+            return jsonify({"msg": "Error with the request data"}), 400
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -18,8 +18,8 @@ class User(db.Model):
     }
 
     def __init__(self, **kwargs):
-        self.email = kwargs.get('email')
-        #self.set_password(kwargs.get('password'))
+        self.email = kwargs['email']
+        #self.set_password(kwargs['password'])
         self.password = kwargs["password"]
         self.username = kwargs["username"]
 
@@ -31,7 +31,7 @@ class User(db.Model):
             # do not serialize the password, its a security breach
         }
 
-class Artist(db.Model):
+class Artist(User, db.Model):
     __tablename__ = 'artist'
     # way to define inheritance
     id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
@@ -41,11 +41,13 @@ class Artist(db.Model):
     age = db.Column(db.SmallInteger, unique=False, nullable=True)
     nationality = db.Column(db.String(40), unique=False, nullable=True)
     bio = db.Column(db.Text, unique=False, nullable=True)
+    # one to many relationships
+        # list of projects
+    projects = db.relationship('Project', backref='project')
 
     __mapper_args__ = {
         'polymorphic_identity':'artist'
     }
-
 
     def __init__(self, **kwargs):
         super().__init__(
@@ -53,21 +55,171 @@ class Artist(db.Model):
             password=kwargs["password"],
             username=kwargs["username"],
         )
-        self.first_name=kwargs["first_name"],
-        self.last_name=kwargs["last_name"],
-        self.age=kwargs["age"],
-        self.nationality=kwargs["nationality"],
-        self.bio=kwargs["bio"],
+        if "first_name" in kwargs:
+            self.first_name=kwargs["first_name"]
+        if "last_name" in kwargs:
+            self.last_name=kwargs["last_name"]
+        if "age" in kwargs:
+            self.age=kwargs["age"]
+        if "nationality" in kwargs:
+            self.nationality=kwargs["nationality"]
+        if "bio" in kwargs:
+            self.bio=kwargs["bio"]
+    
+    @classmethod
+    def create(cls, **kwargs):
+        element = cls(**kwargs)
+        db.session.add(element)
+        try: 
+            db.session.commit()
+        except Exception as error:
+            print(error.args)
+            db.session.rollback()
+            return False
+        return element
 
     def __repr__(self):
         return '<User %r>' % self.username
 
     def serialize(self):
         return {
-            **super.serialize(),
+            **super().serialize(),
             "first_name": self.first_name,
             "last_name": self.last_name,
             "age": self.age,
             "nationality": self.nationality,
             "bio": self.nationality
         }
+
+class Project(db.Model):
+    __tablename__ = 'project'
+    # data
+    id = db.Column(db.Integer, primary_key=True)
+    tittle = db.Column(db.Text, unique=False, nullable=False)
+    header = db.Column(db.Text, unique=False, nullable=True)
+    description = db.Column(db.Text, unique=False, nullable=True)
+        # foreign key to artist
+    artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'))
+    # one to many relationships
+        # list of ids to the files
+    files = db.relationship('FileID', backref='project')
+        # list of versions
+    versions = db.relationship('ProjectVersion', backref='project')
+        #list of polls
+    polls = db.relationship('Poll', backref='project')
+
+    def __init__(self, **kwargs):
+        self.tittle = kwargs['tittle']
+        self.header = kwargs["header"]
+        self.description = kwargs["description"]
+        self.artist_id = kwargs["artist_id"]
+
+    @classmethod
+    def create(cls, **kwargs):
+        element = cls(**kwargs)
+        db.session.add(element)
+        try: 
+            db.session.commit()
+        except Exception as error:
+            print(error.args)
+            db.session.rollback()
+            return False
+        return element
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "artist_id": self.artist_id,
+            "tittle": self.tittle,
+            "header": self.header,
+            "description": self.description 
+        }
+
+class ProjectVersion(db.Model):
+    __tablename__ = 'projectVersion'
+    # data
+    id = db.Column(db.Integer, primary_key=True)
+    tittle = db.Column(db.Text, unique=False, nullable=False)
+    header = db.Column(db.Text, unique=False, nullable=True)
+    description = db.Column(db.Text, unique=False, nullable=True)
+        # foreign key to project
+    project_id = db.Column(db.Integer, db.ForeignKey('project.id'))
+    # one to many relationships
+        # list of id to the files
+    files = db.relationship('FileID', backref='projectVersion')
+
+    def __init__(self, **kwargs):
+        self.tittle = kwargs['tittle']
+        self.header = kwargs["header"]
+        self.description = kwargs["description"]
+        self.project_id = kwargs["project_id"]
+
+    @classmethod
+    def create(cls, **kwargs):
+        element = cls(**kwargs)
+        db.session.add(element)
+        try: 
+            db.session.commit()
+        except Exception as error:
+            print(error.args)
+            db.session.rollback()
+            return False
+        return element
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "project_id": self.project_id,
+            "tittle": self.tittle,
+            "header": self.header,
+            "description": self.description 
+        }
+
+class Poll(db.Model):
+    __tablename__ = 'poll'
+    # data
+    id = db.Column(db.Integer, primary_key=True)
+    info = db.Column(db.JSON)
+        # foreign key to project
+    project_id = db.Column(db.Integer, db.ForeignKey('project.id'))
+
+    # info struct
+    #info = {
+    #   question: 'Is react-polls useful?', 
+    #   pollAnswers: [
+    #       {option: 'Yes', votes: 8},
+    #       {option: 'No', votes: 10}
+    #   ]
+    #}
+    #
+    #
+
+    def __init__(self, **kwargs):
+        self.info = kwargs['info']
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "project_id": self.project_id,
+            **self.info,
+        }
+
+class FileID(db.Model):
+    __tablename__ = 'fileID'
+    # data
+    id = db.Column(db.Integer, primary_key=True)
+        # foreign key to project
+    project_id = db.Column(db.Integer, db.ForeignKey('project.id'))
+        # foreign key to project version
+    project_version_id = db.Column(db.Integer, db.ForeignKey('projectVersion.id'))
+
+    def serialize(self):
+        return_dict = {
+            "id": self.id,
+            "project_id": self.project_id
+        }
+
+        if self.project_id:
+            return_dict["project_id"] = id.project_id
+
+        return return_dict
